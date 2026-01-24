b94fece02910a236cdee08b1876e888f5aac7ec7
Alik9151 <91481295+Alik9151@users.noreply.github.com>
2026-01-24 15:32:52 -0500

    Double limelight align
    changed to effectively use two limelights

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -24,7 +24,7 @@ public final class Constants
     public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(3)), ROBOT_MASS);
     public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
     // Maximum speed of the robot in meters per second, used to limit acceleration.
-    public static final double MAX_SPEED  = 2.5;
+    public static final double MAX_SPEED  = 3.5;
     // Hold time on motor brakes when disabled
     public static final double WHEEL_LOCK_TIME = 10; // seconds
   }
