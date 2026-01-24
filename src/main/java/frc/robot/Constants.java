3abafb1ca853a640e596779bdee7e6f33ce5406d
alawn <leckiqie@gmail.com>
2026-01-24 13:46:09 -0500

    fix swerve drift

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,10 +21,10 @@ public final class Constants
   public static final class DrivebaseConstants
   {
     public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
-    public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+    public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(3)), ROBOT_MASS);
     public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
     // Maximum speed of the robot in meters per second, used to limit acceleration.
-    public static final double MAX_SPEED  = 2;
+    public static final double MAX_SPEED  = 2.5;
     // Hold time on motor brakes when disabled
     public static final double WHEEL_LOCK_TIME = 10; // seconds
   }
