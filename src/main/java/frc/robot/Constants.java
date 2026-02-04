eb8faee591c953a36d0c0774b9ea1e42d37dc4ee
George Gardiner <georgecamg@gmail.com>
2026-02-04 17:46:19 -0500

    Shooter pid. Prob doesn't work

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,6 +18,14 @@ import swervelib.math.Matter;
  */
 public final class Constants
 {
+  public static final class ModuleConstants {
+    public static final double kPShooter=0;
+    public static final double kIShooter=0;
+    public static final double kDShooter=0;
+    public static final double kShooterMotorGearRatio = 1;
+    public static final double kShooterEncoderRot2Rad = 2 * Math.PI * kBucketShooterGearRatio;
+  }
+
   public static final class DrivebaseConstants
   {
     public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,14 +1,17 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import frc.robot.Constants.DriverConstants;
+import frc.robot.Constants.ModuleConstants;
 
 public class ShooterSubsystem extends SubsystemBase {
     private final SparkMax motorA;
@@ -16,6 +19,12 @@ public class ShooterSubsystem extends SubsystemBase {
 
     private final SparkMaxConfig configA;
     private final SparkMaxConfig configB;
+
+    private final RelativeEncoder encoderA;
+    private final RelativeEncoder encoderB;
+
+    private final PIDController PIDA;
+    private final PIDController PIDB;
 
     public ShooterSubsystem() {
         motorA = new SparkMax(DriverConstants.kShooterMotorA, MotorType.kBrushless);
@@ -27,15 +36,23 @@ public class ShooterSubsystem extends SubsystemBase {
         configB.inverted(DriverConstants.kShooterMotorBReversed);
 
         configA.idleMode(IdleMode.kBrake);
+        configA.encoder.positionConversionFactor(ModuleConstants.kShooterEncoderRot2Rad);
         configB.idleMode(IdleMode.kBrake);
+        configB.encoder.positionConversionFactor(ModuleConstants.kShooterEncoderRot2Rad);
 
         motorA.configure(configA, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         motorB.configure(configB, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        encoderA = motorA.getEncoder();
+        encoderB = motorB.getEncoder();
+
+        PIDA = new PIDController(ModuleConstants.kPShooter, ModuleConstants.kIShooter, ModuleConstants.kDShooter);
+        PIDB = new PIDController(ModuleConstants.kPShooter, ModuleConstants.kIShooter, ModuleConstants.kDShooter);
     }
 
     public void start(double speed) {
-      motorA.set(speed);
-      motorB.set(speed);
+      motorA.set(PIDA.calculate(encoderA.getPosition(), speed));
+      motorB.set(PIDB.calculate(encoderB.getPosition(), speed));
     }
 
     public void stop() {
